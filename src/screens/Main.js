8206437924566a182a6d801437fb999a4b3da5bb
asrofhy <asrofiw@users.noreply.github.com>
2020-12-01 01:42:15 +0700

    edit main default

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
-// import {View, Text} from 'react-native';
-
-import Home from './Home';
+import {View, Text} from 'react-native';
 
 export default function Main() {
-  return <Home />;
+  return (
+    <View>
+      <Text>Main</Text>
+    </View>
+  );
 }
