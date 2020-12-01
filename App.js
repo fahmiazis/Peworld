348a0d11445cb5bd7c8c5f69492a8bbea62dd913
Nugroho-00 <snugroho211@gmail.com>
2020-12-01 20:51:35 +0700

    add confirm reset screen mobilr

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import {Provider} from 'react-redux';
 
 import store from './src/redux/store';
 
-import Main from './src/screens/Main';
+import Main from './src/screens/ConfirmPassword';
+// import Main from './src/screens/PencarianScreen';
 import {PersistGate} from 'redux-persist/integration/react';
 
 export default function App() {
