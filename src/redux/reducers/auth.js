8936aa8a011f3215fbfcaa0777425bf627fa77f8
prmstrgt <pramestiregitaa@gmail.com>
2020-12-06 07:42:10 +0700

    bug fixes

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -67,7 +67,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         isLogin: false,
-        token: '',
+        // token: '', jangan di uncomment
         alertMsg: 'Logout success',
         data: {},
       };
